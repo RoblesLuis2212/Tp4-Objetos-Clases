c25d90c3a62bcf7d141190273073b35a5436fe14
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-29 22:56:11 -0300

    Se corrigio el nombre de un archivo, y se agrego la consigna del ejercicio 4

---
+++ js/EjerciciosExtra/Tp4-EjercicioExtra4.js
@@ -0,0 +1,4 @@
+//Crea un objeto alumno con propiedades nombre, curso y calificaciones (un array de números).
+//Escribe una función que reciba un objeto alumno y un número,
+//y que añada la calificación al array de calificaciones del alumno.
+//Calcula la calificación más alta del alumno y clasifica al alumno según su calificación más alta (A para 9-10, B para 7-8, C para 5-6, D para 0-4).
